7e8d58f08ca1341b0287a7e3b9750a27d204178c
DESKTOP-REI9PEB\kosmo <nahs001@naver.com>
2023-05-13 16:27:15 +0900

    이미지 업로드 시 로딩화면 구현

--- zTeamProject/src/main/webapp/resources/js/class_form.js
+++ zTeamProject/src/main/webapp/resources/js/class_form.js
@@ -28,6 +28,7 @@
     				console.log(data);
     				//console.log(data.url);
     				//alert(data);
+    				test('로딩중'); // 로딩이미지 호출
     				setTimeout(function() { // 이미지가 업로드되는 시간이 이미지를 불러내는 시간보다 늦어져서 404에러가 뜨기때문에 업로드 시작 5초 후에 이미지를 불러오게한다.
     				$('#summernote').summernote("insertImage",data); // 저장된 이미지 url을 썸머노트에 넣는다.
     				var path = $('#path').val();
@@ -220,3 +221,44 @@
 	 
 	    
     });
+    
+        function test(imageName) {
+    LoadingWithMask('http://192.168.0.68:8080/zTeamProject/resources/uploads/Spinner_loading.gif');
+    setTimeout("closeLoadingWithMask()", 4500);
+}
+ 
+function LoadingWithMask(gif) {
+    //화면의 높이와 너비를 구합니다.
+    var maskHeight = $(document).height();
+    var maskWidth  = window.document.body.clientWidth;
+     
+    //화면에 출력할 마스크를 설정해줍니다.
+    var mask       = "<div id='mask' style='position:absolute; z-index:9000; background-color:#000000; display:none; left:0; top:0;'></div>";
+    var loadingImg = '';
+     
+    loadingImg += "<div id='loadingImg'>";
+    loadingImg += " <img src='"+ gif + "' style='position: absolute; display: block; margin: 0px auto;'/>";
+ 	loadingImg += "</div>";
+ 	
+    //화면에 레이어 추가
+    $('body')
+        .append(mask)
+ 
+    //마스크의 높이와 너비를 화면 것으로 만들어 전체 화면을 채웁니다.
+    $('#mask').css({
+            'width' : maskWidth,
+            'height': maskHeight,
+            'opacity' : '0.3'
+    }); 
+  
+    //마스크 표시
+    $('#mask').show();
+  
+    //로딩중 이미지 표시
+    $('#mask').append(loadingImg);
+}
+ 
+function closeLoadingWithMask() {
+    $('#mask, #loadingImg').hide();
+    $('#mask, #loadingImg').empty();  
+}
